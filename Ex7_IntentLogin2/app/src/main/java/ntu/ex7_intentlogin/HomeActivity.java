177b765cbe5e8a246a05614bb7eec2f3e505bd0c
khoa373289 <khoatran2072004@gmail.com>
2025-03-07 16:01:51 +0700

    Ex7
    Đã Onclick tới iLogin và SetUp sự kiện

--- Ex7_IntentLogin2/app/src/main/java/ntu/ex7_intentlogin/HomeActivity.java
+++ Ex7_IntentLogin2/app/src/main/java/ntu/ex7_intentlogin/HomeActivity.java
@@ -1,9 +1,6 @@
 package ntu.ex7_intentlogin;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,28 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        //Tim dieu khien
-        Button btnDangNhap = (Button) findViewById(R.id.btnLogin);
-        //Su kien
-        btnDangNhap.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Lenh xu li
-                Intent iLogin = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(iLogin);
-            }
         });
     }
 }
