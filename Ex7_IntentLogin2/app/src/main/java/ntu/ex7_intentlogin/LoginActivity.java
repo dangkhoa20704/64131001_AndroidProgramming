ff4b2f2d603bb7c3c362d0c6280a794f959e9a53
khoa373289 <khoatran2072004@gmail.com>
2025-03-07 16:24:18 +0700

    Ex7
    Đã xong Login để vào Home

--- Ex7_IntentLogin2/app/src/main/java/ntu/ex7_intentlogin/LoginActivity.java
+++ Ex7_IntentLogin2/app/src/main/java/ntu/ex7_intentlogin/LoginActivity.java
@@ -1,6 +1,11 @@
 package ntu.ex7_intentlogin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +24,33 @@ public class LoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button btnXacNhan = (Button) findViewById(R.id.btnOk);
+
+        btnXacNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Xu li dang nhap
+                //LAY DU LIEU
+                EditText edTenDN = (EditText) findViewById(R.id.edtName);
+                EditText edPass = (EditText) findViewById(R.id.edtPass);
+                EditText edEmail = (EditText) findViewById(R.id.edtEmail);
+                //b2.Lay du lieu
+                String tenDangNhap = edTenDN.getText().toString();
+                String mk = edPass.getText().toString();
+                //b3. Kiem tra mat khau
+                if(tenDangNhap.equals("builedangkhoa") && mk.equals("Khoa20704")){
+                    Intent iQuiz = new Intent(LoginActivity.this, HomeActivity.class);
+                    //Goi du lieu vao iQuiz
+                    iQuiz.putExtra("ten_dang_nhap",tenDangNhap);
+                    iQuiz.putExtra("mk_dang_nhap",mk);
+                    //Gui di
+                    startActivity(iQuiz);
+                }
+                else{
+                    Toast.makeText(LoginActivity.this, "Tên đăng nhập hoặc mật khẩu sai !",Toast.LENGTH_LONG);
+                }
+            }
         });
     }
 }
