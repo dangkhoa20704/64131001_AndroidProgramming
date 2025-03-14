34107fcc00c3d65da92744a5fe60b8bfc0e717ac
khoa373289 <khoatran2072004@gmail.com>
2025-03-14 15:18:49 +0700

    LoginPage
    Hoàn thành xong điều khiển lắng nghe

--- LoginPage/app/src/main/java/bill/edu/loginpage/MainActivity.java
+++ LoginPage/app/src/main/java/bill/edu/loginpage/MainActivity.java
@@ -1,6 +1,10 @@
 package bill.edu.loginpage;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +14,15 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText username;
+    EditText password;
+    Button btnLogin;
+
+    void TimDieuKhien(){
+        username = findViewById(R.id.editUser);
+        password = findViewById(R.id.editPass);
+        btnLogin = findViewById(R.id.btnLogin);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +32,18 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        TimDieuKhien();
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("Dang Khoa") && password.getText().toString().equals("1234")){
+                    Toast.makeText(MainActivity.this, "Đăng nhập thành công !",Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(MainActivity.this,"Tên đăng nhập hoặc mật khẩu sai !",Toast.LENGTH_LONG).show();
+                }
+            }
         });
     }
 }
