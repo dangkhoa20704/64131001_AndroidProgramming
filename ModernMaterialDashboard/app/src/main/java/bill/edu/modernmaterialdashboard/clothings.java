b9dcabd9179da0901497db149cdc1c520fb9061c
khoa373289 <khoatran2072004@gmail.com>
2025-03-17 14:31:37 +0700

    Modern
    Update code

--- ModernMaterialDashboard/app/src/main/java/bill/edu/modernmaterialdashboard/clothings.java
+++ ModernMaterialDashboard/app/src/main/java/bill/edu/modernmaterialdashboard/clothings.java
@@ -1,6 +1,6 @@
 package bill.edu.modernmaterialdashboard;
 
-import android.content.Intent;
+import android.os.Bundle;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -10,33 +10,21 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    CardView clothingCard;
-    void TimDieuKhien(){
-        clothingCard = findViewById(R.id.clothingCard);
-    }
+public class clothings extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_clothings);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        TimDieuKhien();
-        clothingCard.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, clothings.class);
-                startActivity(intent);
-            }
         });
     }
 }
