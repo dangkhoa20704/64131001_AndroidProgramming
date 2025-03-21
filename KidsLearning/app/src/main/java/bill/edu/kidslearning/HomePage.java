89a923f82f187ada7a4a7d91a9c3f6163b4b3f38
khoa373289 <khoatran2072004@gmail.com>
2025-03-21 13:59:54 +0700

    MathsIsFun
    Em gộp hai bài chung một project ạ.
    Em làm xong trang chọn ngôn ngữ.

--- KidsLearning/app/src/main/java/bill/edu/kidslearning/HomePage.java
+++ KidsLearning/app/src/main/java/bill/edu/kidslearning/HomePage.java
@@ -1,6 +1,10 @@
 package bill.edu.kidslearning;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +13,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class HomePage extends AppCompatActivity {
-
+    ImageButton Maths;
+    void TimDieuKhien(){
+        Maths = findViewById(R.id.btnMath);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +27,14 @@ public class HomePage extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        TimDieuKhien();
+        Maths.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentMaths = new Intent(HomePage.this, MathsApp.class);
+                startActivity(intentMaths);
+            }
+        });
+
     }
 }
