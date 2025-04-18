c0def04da468cd99a6f9239f74da30f1466f29d2
khoa373289 <khoatran2072004@gmail.com>
2025-04-18 12:35:50 +0700

    Fragment_Replace
    Xong replace ba màn hình

--- Fragment_Replace/app/src/main/java/bill/edu/fragment_replace/FooterFragment.java
+++ Fragment_Replace/app/src/main/java/bill/edu/fragment_replace/FooterFragment.java
@@ -3,10 +3,13 @@ package bill.edu.fragment_replace;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 public class FooterFragment extends Fragment {
 
     @Override
@@ -20,6 +23,40 @@ public class FooterFragment extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_footer, container, false);
         // Inflate the layout for this fragment
+        Button b1 = v.findViewById(R.id.button);
+        Button b2 = v.findViewById(R.id.button2);
+        Button b3 = v.findViewById(R.id.button3);
+
+        FragmentManager fragmentManager = getParentFragmentManager();
+
+        //Event
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView_Content, new Fragment1())
+                        .commit();
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView_Content, new Fragment2())
+                        .commit();
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView_Content, new Fragment3())
+                        .commit();
+            }
+        });
+
        return v;
     }
 }
